0162951fc22e7ebc6b829261b4e694d8a607ef79
Andrea Barghigiani <andrea@codeat.it>
2018-08-21 19:42:11 +0200

    Initial commit

---
+++ sam-gutenberg.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Plugin's bootstrap file to launch the plugin.
+ *
+ * @package     SAM_Gutenberg
+ * @author      Andrea Barghigiani
+ * @license     GPL2+
+ *
+ * @wordpress-plugin
+ * Plugin Name: SAM Gutenberg Blocks
+ * Plugin URI:  https://gutenberg.courses
+ * Description: A plugin containing example blocks for developers.  From <a href="https://gutenberg.courses/development">Zac Gordon's Gutenberg Development Course</a>.
+ * Version:     2.1.0
+ * Author:      Zac Gordon
+ * Author URI:  https://twitter.com/zgordon
+ * Text Domain: jsforwpblocks
+ * Domain Path: /languages
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// namespace SAM_Gutenberg;
+namespace SAM_Gutenberg;
+
+//  Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+/**
+ * Gets this plugin's absolute directory path.
+ *
+ * @since  2.1.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_directory() {
+	return __DIR__;
+}
+
+/**
+ * Gets this plugin's URL.
+ *
+ * @since  2.1.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_url() {
+	static $plugin_url;
+
+	if ( empty( $plugin_url ) ) {
+		$plugin_url = plugins_url( null, __FILE__ );
+	}
+
+	return $plugin_url;
+}
+
+// Enqueue JS and CSS
+include __DIR__ . '/lib/enqueue-scripts.php';
+
+// Register meta boxes
+include __DIR__ . '/lib/meta-boxes.php';
+
+// Block Templates
+include __DIR__ . '/lib/block-templates.php';
+
+// Dynamic Blocks
+include __DIR__ . '/blocks/12-dynamic/index.php';
